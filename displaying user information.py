b5daf5f007d719ae6c1dbedceb363a96dbd40831
Qhamaninande Rebe <67279129+QueMaster@users.noreply.github.com>
2021-07-20 22:15:28 +0200

    Add files via upload

---
+++ displaying user information.py
@@ -0,0 +1,28 @@
+#using pyhon simple variables.
+#Assigning values to variables.
+
+name = "Qhamaninande"
+surname = "Rebe"
+age = 25
+append = "I am turning "
+idNumber = "960831 0000 000"
+location = "South Africa"
+occupation = "Student"
+cell = "079 0000 123/081 ### 0000"
+
+#printing out the values entered 
+
+print ("What is your name & surname")
+print (name, surname)
+print (" ")
+print("What is", name, "Age")
+print(append, age)
+print(" ")
+print("Give me your ID & Location")
+print(idNumber,location)
+print(" ")
+print("What are currently doing now")
+print(occupation)
+print(" ")
+print("The foolowing are the contact numbers")
+print(cell)
